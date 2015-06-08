99b3bfa87d5738fa3c709f7a2bc5e3a1f8ab5dd4
Paul Bill <paul@develodesign.co.uk>
2015-06-08 16:24:53 +0100

    adding examples

---
+++ examples/test.js
@@ -0,0 +1,14 @@
+/**
+ * Example video test
+ *
+ * @copyright   (c) Develo Design 2015
+ * @author      paul
+ * @package     play-on-scroll
+ * @date        08/06/15
+ */
+jQuery( function( $ ){
+
+	'use strict';
+
+	$( 'video' ).playOnScroll();
+});
